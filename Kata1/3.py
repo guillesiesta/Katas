ecc4b47e82ea31ed7c47dbedf593a0995c2d2553
guillesiesta <guille.siesta.muriel@gmail.com>
2020-05-16 12:33:52 +0200

    que te dise canio un programita mas

---
+++ Kata1/3.py
@@ -0,0 +1,9 @@
+edad = input("Dime tu edad o te reviento la cabeza a hostias: ")
+edad = int(edad)
+
+if edad < 4:
+    print("Puedes entrar a drogarte gratis, niñato.")
+elif edad >=4 and edad<=18:
+    print("A pagar 5 pavos espabilao")
+else:
+    print("Por tener más años que un saco de gnomos a pagar 10 pavos")
